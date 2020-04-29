01d404d784ca9643ca69967e8662649728504beb
sameer <sameer.gmail.com>
2020-04-29 18:16:05 +0530

    project files

---
+++ src/main/java/sam/raj/App.java
@@ -0,0 +1,13 @@
+package sam.raj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
